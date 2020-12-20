d36efb010e6378e5dfa0250e61ddf0ad4557c80d
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-12-20 16:02:04 +0900

    [UPDATE] 友達追加時イベント追加 & 説明変数を追加jj:w

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-#環境変数取得
+# 環境変数
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
+# API
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+# テスト用
 @app.route("/")
 def hello_world():
     return "hello world!"
 
+# Webhookからのリクエストチェック
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
+    # LINEサーバからのアクセスなのか検証
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
@@ -35,13 +39,21 @@ def callback():
 
     return 'OK'
 
+# オウム返しプログラム
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    received_msg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=received_msg))
+
+# 友達追加時イベント
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Thank you for the friend request.'))
 
 if __name__ == "__main__":
-#    app.run()
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
