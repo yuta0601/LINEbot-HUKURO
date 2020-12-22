bb3c71a821c5cc29feae9e56064556fd86aa9e19
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-12-23 03:11:49 +0900

    動かない

--- main.py
+++ main.py
@@ -35,81 +35,81 @@ CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-payload = {
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Arm Chair, White",
-        "weight": "bold",
-        "size": "xl",
-        "wrap": true,
-        "contents": []
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "contents": [
-          {
-            "type": "text",
-            "text": "$49",
-            "weight": "bold",
-            "size": "xl",
-            "flex": 0,
-            "wrap": true,
-            "contents": []
-          },
-          {
-            "type": "text",
-            "text": ".99",
-            "weight": "bold",
-            "size": "sm",
-            "flex": 0,
-            "wrap": true,
-            "contents": []
-          }
-        ]
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "Add to Cart",
-          "uri": "https://linecorp.com"
-        },
-        "style": "primary"
-      },
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "Add to wishlist",
-          "uri": "https://linecorp.com"
-        }
-      }
-    ]
-  }
-}
+# payload = {
+#   "type": "bubble",
+#   "hero": {
+#     "type": "image",
+#     "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+#     "size": "full",
+#     "aspectRatio": "20:13",
+#     "aspectMode": "cover"
+#   },
+#   "body": {
+#     "type": "box",
+#     "layout": "vertical",
+#     "spacing": "sm",
+#     "contents": [
+#       {
+#         "type": "text",
+#         "text": "Arm Chair, White",
+#         "weight": "bold",
+#         "size": "xl",
+#         "wrap": true,
+#         "contents": []
+#       },
+#       {
+#         "type": "box",
+#         "layout": "baseline",
+#         "contents": [
+#           {
+#             "type": "text",
+#             "text": "$49",
+#             "weight": "bold",
+#             "size": "xl",
+#             "flex": 0,
+#             "wrap": true,
+#             "contents": []
+#           },
+#           {
+#             "type": "text",
+#             "text": ".99",
+#             "weight": "bold",
+#             "size": "sm",
+#             "flex": 0,
+#             "wrap": true,
+#             "contents": []
+#           }
+#         ]
+#       }
+#     ]
+#   },
+#   "footer": {
+#     "type": "box",
+#     "layout": "vertical",
+#     "spacing": "sm",
+#     "contents": [
+#       {
+#         "type": "button",
+#         "action": {
+#           "type": "uri",
+#           "label": "Add to Cart",
+#           "uri": "https://linecorp.com"
+#         },
+#         "style": "primary"
+#       },
+#       {
+#         "type": "button",
+#         "action": {
+#           "type": "uri",
+#           "label": "Add to wishlist",
+#           "uri": "https://linecorp.com"
+#         }
+#       }
+#     ]
+#   }
+# }
 
-container_obj = FlexSendMessage.new_from_json_dict(payload)
+# container_obj = FlexSendMessage.new_from_json_dict(payload)
 
 @app.route("/")
 def hello_world():
@@ -148,11 +148,11 @@ def handle_message(event):
             TextSendMessage(text="https://sites.google.com/site/is13hp/home"),
         )
     
-    elif event.message.text = "test":
-        line_bot_api.reply_message(
-            event.reply_token, 
-            TextSendMessage(text=container_obj),
-        )
+    # elif event.message.text = "test":
+    #     line_bot_api.reply_message(
+    #         event.reply_token, 
+    #         TextSendMessage(text=container_obj),
+    #     )
 
     else:
         line_bot_api.reply_message(
