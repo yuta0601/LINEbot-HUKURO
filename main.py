1d434070339600a89afcfea80de691fde2ae07d1
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-12-23 03:03:42 +0900

    バブルに変更

--- main.py
+++ main.py
@@ -36,188 +36,77 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 payload = {
-  "type": "carousel",
-  "contents": [
-    {
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover"
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Arm Chair, White",
+        "weight": "bold",
+        "size": "xl",
+        "wrap": true,
+        "contents": []
       },
-      "body": {
+      {
         "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
+        "layout": "baseline",
         "contents": [
           {
             "type": "text",
-            "text": "Arm Chair, White",
+            "text": "$49",
             "weight": "bold",
             "size": "xl",
-            "wrap": True,
-            "contents": []
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "text",
-                "text": "$49",
-                "weight": "bold",
-                "size": "xl",
-                "flex": 0,
-                "wrap": True,
-                "contents": []
-              },
-              {
-                "type": "text",
-                "text": ".99",
-                "weight": "bold",
-                "size": "sm",
-                "flex": 0,
-                "wrap": True,
-                "contents": []
-              }
-            ]
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to Cart",
-              "uri": "https://linecorp.com"
-            },
-            "style": "primary"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to wishlist",
-              "uri": "https://linecorp.com"
-            }
-          }
-        ]
-      }
-    },
-    {
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Metal Desk Lamp",
-            "weight": "bold",
-            "size": "xl",
-            "wrap": True,
-            "contents": []
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "flex": 1,
-            "contents": [
-              {
-                "type": "text",
-                "text": "$11",
-                "weight": "bold",
-                "size": "xl",
-                "flex": 0,
-                "wrap": True,
-                "contents": []
-              },
-              {
-                "type": "text",
-                "text": ".99",
-                "weight": "bold",
-                "size": "sm",
-                "flex": 0,
-                "wrap": True,
-                "contents": []
-              }
-            ]
-          },
-          {
-            "type": "text",
-            "text": "Temporarily out of stock",
-            "size": "xxs",
-            "color": "#FF5551",
             "flex": 0,
-            "margin": "md",
-            "wrap": True,
+            "wrap": true,
+            "contents": []
+          },
+          {
+            "type": "text",
+            "text": ".99",
+            "weight": "bold",
+            "size": "sm",
+            "flex": 0,
+            "wrap": true,
             "contents": []
           }
         ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "Add to Cart",
+          "uri": "https://linecorp.com"
+        },
+        "style": "primary"
       },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to Cart",
-              "uri": "https://linecorp.com"
-            },
-            "flex": 2,
-            "color": "#AAAAAA",
-            "style": "primary"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to wish list",
-              "uri": "https://linecorp.com"
-            }
-          }
-        ]
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "Add to wishlist",
+          "uri": "https://linecorp.com"
+        }
       }
-    },
-    {
-      "type": "bubble",
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "See more",
-              "uri": "https://linecorp.com"
-            },
-            "flex": 1,
-            "gravity": "center"
-          }
-        ]
-      }
-    }
-  ]
+    ]
+  }
 }
 
 container_obj = FlexSendMessage.new_from_json_dict(payload)
