0c03e87e6204963b1a91453cd6f3447c6ddc24ff
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-12-23 13:42:58 +0900

    flexadd

--- main.py
+++ main.py
@@ -35,77 +35,233 @@ CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+# payload = {
+#   "type": "bubble",
+#   "hero": {
+#     "type": "image",
+#     "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+#     "size": "full",
+#     "aspectRatio": "20:13",
+#     "aspectMode": "cover"
+#   },
+#   "body": {
+#     "type": "box",
+#     "layout": "vertical",
+#     "spacing": "sm",
+#     "contents": [
+#       {
+#         "type": "text",
+#         "text": "Arm Chair, White",
+#         "weight": "bold",
+#         "size": "xl",
+#         "wrap": true,
+#         "contents": []
+#       },
+#       {
+#         "type": "box",
+#         "layout": "baseline",
+#         "contents": [
+#           {
+#             "type": "text",
+#             "text": "$49",
+#             "weight": "bold",
+#             "size": "xl",
+#             "flex": 0,
+#             "wrap": true,
+#             "contents": []
+#           },
+#           {
+#             "type": "text",
+#             "text": ".99",
+#             "weight": "bold",
+#             "size": "sm",
+#             "flex": 0,
+#             "wrap": true,
+#             "contents": []
+#           }
+#         ]
+#       }
+#     ]
+#   },
+#   "footer": {
+#     "type": "box",
+#     "layout": "vertical",
+#     "spacing": "sm",
+#     "contents": [
+#       {
+#         "type": "button",
+#         "action": {
+#           "type": "uri",
+#           "label": "Add to Cart",
+#           "uri": "https://linecorp.com"
+#         },
+#         "style": "primary"
+#       },
+#       {
+#         "type": "button",
+#         "action": {
+#           "type": "uri",
+#           "label": "Add to wishlist",
+#           "uri": "https://linecorp.com"
+#         }
+#       }
+#     ]
+#   }
+# }
+
 payload = {
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Arm Chair, White",
-        "weight": "bold",
-        "size": "xl",
-        "wrap": true,
-        "contents": []
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "contents": [
-          {
-            "type": "text",
-            "text": "$49",
-            "weight": "bold",
-            "size": "xl",
-            "flex": 0,
-            "wrap": true,
-            "contents": []
-          },
-          {
-            "type": "text",
-            "text": ".99",
-            "weight": "bold",
-            "size": "sm",
-            "flex": 0,
-            "wrap": true,
-            "contents": []
-          }
-        ]
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "hero": {
+      "type": "image",
+      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+      "size": "full",
+      "aspectRatio": "20:13",
+      "aspectMode": "cover",
+      "action": {
+        "type": "uri",
+        "label": "Line",
+        "uri": "https://linecorp.com/"
       }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "Add to Cart",
-          "uri": "https://linecorp.com"
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "Brown Cafe",
+          "size": "xl",
+          "weight": "bold"
         },
-        "style": "primary"
-      },
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "Add to wishlist",
-          "uri": "https://linecorp.com"
+        {
+          "type": "box",
+          "layout": "baseline",
+          "margin": "md",
+          "contents": [
+            {
+              "type": "icon",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+              "size": "sm"
+            },
+            {
+              "type": "icon",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+              "size": "sm"
+            },
+            {
+              "type": "icon",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+              "size": "sm"
+            },
+            {
+              "type": "icon",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+              "size": "sm"
+            },
+            {
+              "type": "icon",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+              "size": "sm"
+            },
+            {
+              "type": "text",
+              "text": "4.0",
+              "flex": 0,
+              "margin": "md",
+              "size": "sm",
+              "color": "#999999"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "margin": "lg",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Place",
+                  "flex": 1,
+                  "size": "sm",
+                  "color": "#AAAAAA"
+                },
+                {
+                  "type": "text",
+                  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                  "flex": 5,
+                  "size": "sm",
+                  "color": "#666666",
+                  "wrap": True
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Time",
+                  "flex": 1,
+                  "size": "sm",
+                  "color": "#AAAAAA"
+                },
+                {
+                  "type": "text",
+                  "text": "10:00 - 23:00",
+                  "flex": 5,
+                  "size": "sm",
+                  "color": "#666666",
+                  "wrap": True
+                }
+              ]
+            }
+          ]
         }
-      }
-    ]
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "vertical",
+      "flex": 0,
+      "spacing": "sm",
+      "contents": [
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "CALL",
+            "uri": "https://linecorp.com"
+          },
+          "height": "sm",
+          "style": "link"
+        },
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "WEBSITE",
+            "uri": "https://linecorp.com"
+          },
+          "height": "sm",
+          "style": "link"
+        },
+        {
+          "type": "spacer",
+          "size": "sm"
+        }
+      ]
+    }
   }
 }
 
