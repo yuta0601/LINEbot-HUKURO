a8866d55bc12aaa653b9043589581108a590180f
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-12-25 18:33:46 +0900

    test

--- main.py
+++ main.py
@@ -36,122 +36,124 @@ CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-# payload = {
-#   "type": "bubble",
-#   "hero": {
-#     "type": "image",
-#     "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-#     "size": "full",
-#     "aspectRatio": "20:13",
-#     "aspectMode": "cover"
-#   },
-#   "body": {
-#     "type": "box",
-#     "layout": "vertical",
-#     "spacing": "sm",
-#     "contents": [
-#       {
-#         "type": "text",
-#         "text": "Arm Chair, White",
-#         "weight": "bold",
-#         "size": "xl",
-#         "wrap": true,
-#         "contents": []
-#       },
-#       {
-#         "type": "box",
-#         "layout": "baseline",
-#         "contents": [
-#           {
-#             "type": "text",
-#             "text": "$49",
-#             "weight": "bold",
-#             "size": "xl",
-#             "flex": 0,
-#             "wrap": true,
-#             "contents": []
-#           },
-#           {
-#             "type": "text",
-#             "text": ".99",
-#             "weight": "bold",
-#             "size": "sm",
-#             "flex": 0,
-#             "wrap": true,
-#             "contents": []
-#           }
-#         ]
-#       }
-#     ]
-#   },
-#   "footer": {
-#     "type": "box",
-#     "layout": "vertical",
-#     "spacing": "sm",
-#     "contents": [
-#       {
-#         "type": "button",
-#         "action": {
-#           "type": "uri",
-#           "label": "Add to Cart",
-#           "uri": "https://linecorp.com"
-#         },
-#         "style": "primary"
-#       },
-#       {
-#         "type": "button",
-#         "action": {
-#           "type": "uri",
-#           "label": "Add to wishlist",
-#           "uri": "https://linecorp.com"
-#         }
-#       }
-#     ]
-#   }
-# }
-
-flex_message_json_string="""
-{  
-  type: flex,
-  altText: Flex Message,
-  contents: {
-    type: bubble,
-    direction: ltr,
-    header: {
-      type: box,
-      layout: vertical,
-      contents: [
-        {
-          type: text,
-          text: ,
-          align: center
-        }
-      ]
-    },
-    hero: {
-      type: image,
-      url: https://images-na.ssl-images-amazon.com/images/I/81dkWPlc-AL._SX522_.jpg,
-      size: full,
-      aspectRatio: 1.51:1,
-      aspectMode: fit
-    },
-    footer: {
-      type: box,
-      layout: horizontal,
-      contents: [
-        {
-          type: button,
-          action: {
-            type: uri,
-            label: 購入,
-            uri: https://www.amazon.co.jp/天然水-【Amazon-co-jp-限定】サントリー-南アルプスの天然水-2L×9本/dp/B07F1CCSJZ/ref=sr_1_1_sspa?__mk_ja_JP=カタカナ&keywords=水&qid=1563628084&s=gateway&sr=8-1-spons&psc=1
+flex_message_json_string = """
+{
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Arm Chair, White",
+        "weight": "bold",
+        "size": "xl",
+        "wrap": true,
+        "contents": []
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "text",
+            "text": "$49",
+            "weight": "bold",
+            "size": "xl",
+            "flex": 0,
+            "wrap": true,
+            "contents": []
+          },
+          {
+            "type": "text",
+            "text": ".99",
+            "weight": "bold",
+            "size": "sm",
+            "flex": 0,
+            "wrap": true,
+            "contents": []
           }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "Add to Cart",
+          "uri": "https://linecorp.com"
+        },
+        "style": "primary"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "Add to wishlist",
+          "uri": "https://linecorp.com"
         }
-      ]
-    }
+      }
+    ]
   }
 }
 """
+
+# flex_message_json_string="""
+# {  
+#   type: bubble,
+#   altText: Flex Message,
+#   contents: {
+#     type: bubble,
+#     direction: ltr,
+#     header: {
+#       type: box,
+#       layout: vertical,
+#       contents: [
+#         {
+#           type: text,
+#           text: ,
+#           align: center
+#         }
+#       ]
+#     },
+#     hero: {
+#       type: image,
+#       url: https://images-na.ssl-images-amazon.com/images/I/81dkWPlc-AL._SX522_.jpg,
+#       size: full,
+#       aspectRatio: 1.51:1,
+#       aspectMode: fit
+#     },
+#     footer: {
+#       type: box,
+#       layout: horizontal,
+#       contents: [
+#         {
+#           type: button,
+#           action: {
+#             type: uri,
+#             label: 購入,
+#             uri: https://www.amazon.co.jp/天然水-【Amazon-co-jp-限定】サントリー-南アルプスの天然水-2L×9本/dp/B07F1CCSJZ/ref=sr_1_1_sspa?__mk_ja_JP=カタカナ&keywords=水&qid=1563628084&s=gateway&sr=8-1-spons&psc=1
+#           }
+#         }
+#       ]
+#     }
+#   }
+# }
+# """
 flex_message_json_dict = json.loads(flex_message_json_string)
 
 # payload = """
@@ -365,7 +367,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             FlexSendMessage(
-                alt_text=alt_text,
+                alt_text="test",
                 contents=flex_message_json_dict
             )
         )
