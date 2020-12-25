f496a6a6e3f435a83d1fa593b944de46b7d336d4
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-12-26 00:27:05 +0900

    ロールバック

--- main.py
+++ main.py
@@ -26,6 +26,57 @@ from linebot.models import (
     AudioMessage,
 )
 
+# from linebot.models import (
+#     MessageEvent,
+#     TextMessage,
+#     TextSendMessage,
+#     SourceUser,
+#     SourceGroup,
+#     SourceRoom,
+#     TemplateSendMessage,
+#     ConfirmTemplate,
+#     MessageAction,
+#     ButtonsTemplate,
+#     ImageCarouselTemplate,
+#     ImageCarouselColumn,
+#     URIAction,
+#     PostbackAction,
+#     DatetimePickerAction,
+#     CameraAction,
+#     CameraRollAction,
+#     LocationAction,
+#     CarouselTemplate,
+#     CarouselColumn,
+#     PostbackEvent,
+#     StickerMessage,
+#     StickerSendMessage,
+#     LocationMessage,
+#     LocationSendMessage,
+#     ImageMessage,
+#     VideoMessage,
+#     AudioMessage,
+#     FileMessage,
+#     UnfollowEvent,
+#     FollowEvent,
+#     JoinEvent,
+#     LeaveEvent,
+#     BeaconEvent,
+#     MemberJoinedEvent,
+#     MemberLeftEvent,
+#     FlexSendMessage,
+#     BubbleContainer,
+#     ImageComponent,
+#     BoxComponent,
+#     TextComponent,
+#     SpacerComponent,
+#     IconComponent,
+#     ButtonComponent,
+#     SeparatorComponent,
+#     QuickReply,
+#     QuickReplyButton,
+#     ImageSendMessage,
+# )
+
 app = Flask(__name__)
 
 # 環境変数
@@ -36,81 +87,81 @@ CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-flex_message_json_string = """
-{
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Arm Chair, White",
-        "weight": "bold",
-        "size": "xl",
-        "wrap": true,
-        "contents": []
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "contents": [
-          {
-            "type": "text",
-            "text": "$49",
-            "weight": "bold",
-            "size": "xl",
-            "flex": 0,
-            "wrap": true,
-            "contents": []
-          },
-          {
-            "type": "text",
-            "text": ".99",
-            "weight": "bold",
-            "size": "sm",
-            "flex": 0,
-            "wrap": true,
-            "contents": []
-          }
-        ]
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "Add to Cart",
-          "uri": "https://linecorp.com"
-        },
-        "style": "primary"
-      },
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "Add to wishlist",
-          "uri": "https://linecorp.com"
-        }
-      }
-    ]
-  }
-}
-"""
+# flex_message_json_string = """
+# {
+#   "type": "bubble",
+#   "hero": {
+#     "type": "image",
+#     "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+#     "size": "full",
+#     "aspectRatio": "20:13",
+#     "aspectMode": "cover"
+#   },
+#   "body": {
+#     "type": "box",
+#     "layout": "vertical",
+#     "spacing": "sm",
+#     "contents": [
+#       {
+#         "type": "text",
+#         "text": "Arm Chair, White",
+#         "weight": "bold",
+#         "size": "xl",
+#         "wrap": true,
+#         "contents": []
+#       },
+#       {
+#         "type": "box",
+#         "layout": "baseline",
+#         "contents": [
+#           {
+#             "type": "text",
+#             "text": "$49",
+#             "weight": "bold",
+#             "size": "xl",
+#             "flex": 0,
+#             "wrap": true,
+#             "contents": []
+#           },
+#           {
+#             "type": "text",
+#             "text": ".99",
+#             "weight": "bold",
+#             "size": "sm",
+#             "flex": 0,
+#             "wrap": true,
+#             "contents": []
+#           }
+#         ]
+#       }
+#     ]
+#   },
+#   "footer": {
+#     "type": "box",
+#     "layout": "vertical",
+#     "spacing": "sm",
+#     "contents": [
+#       {
+#         "type": "button",
+#         "action": {
+#           "type": "uri",
+#           "label": "Add to Cart",
+#           "uri": "https://linecorp.com"
+#         },
+#         "style": "primary"
+#       },
+#       {
+#         "type": "button",
+#         "action": {
+#           "type": "uri",
+#           "label": "Add to wishlist",
+#           "uri": "https://linecorp.com"
+#         }
+#       }
+#     ]
+#   }
+# }
+# """
 
 # flex_message_json_string="""
 # {  
@@ -154,7 +205,7 @@ flex_message_json_string = """
 #   }
 # }
 # """
-flex_message_json_dict = json.loads(flex_message_json_string)
+# flex_message_json_dict = json.loads(flex_message_json_string)
 
 # payload = """
 # {
