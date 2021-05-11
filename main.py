c8460ecb7efdb5cd815473b8c982faf31c3d804e
yuta0601 <y.nakazawa3003+github@gmail.com>
2021-05-11 22:36:08 +0900

    [UPDATE]

--- main.py
+++ main.py
@@ -73,12 +73,12 @@ def handle_message(event):
         status_message = profile.status_message
 
         # グループ情報を取得
-        groupID = event.source.group_id
+        groupId = event.source.group_id
 
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="test"),
+            TextSendMessage(text=groupId),
         )
 
 
